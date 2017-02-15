cd71d69733a25d8eb4fd04dc6eeb7e88a8981025
Cara Dippenaar <caradippenaar@MacBook-Air.local>
2017-02-15 14:52:30 +0200

    Happy World first commit

---
+++ HappyWorld/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  HappyWorld
+//
+//  Created by Cara Dippenaar on 2017/02/15.
+//  Copyright © 2017 Bloc. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        
+        print("Hello Happy world")
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ HappyWorldTests/HappyWorldTests.swift
@@ -0,0 +1,36 @@
+//
+//  HappyWorldTests.swift
+//  HappyWorldTests
+//
+//  Created by Cara Dippenaar on 2017/02/15.
+//  Copyright © 2017 Bloc. All rights reserved.
+//
+
+import XCTest
+@testable import HappyWorld
+
+class HappyWorldTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
